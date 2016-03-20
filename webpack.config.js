b238679230d4585c6a02087a0e1925a04f4f16b1
Olivier Mungo <olivier@mungo.eu>
2016-03-20 20:24:09 +0100

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 var os = require('os');
 
 module.exports = {
-  entry: "./src/boot.ts",
+  entry: './src/boot.ts',
   output: {
-    filename: "bundle.js",
-    publicPath: "http://" + getIp() + ":8080/"
+    filename: 'bundle.js',
+    publicPath: 'http://' + getIp() + ':8080/'
   },
   devtool: 'source-map',
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts',  '.js', '.css', '.html', '.js' ],
     alias: {
-      "font-awesome": "font-awesome/css/font-awesome.min.css"
+      'font-awesome': 'font-awesome/css/font-awesome.min.css'
     } 
   },
   module: {
     loaders: [
       { test: /\.ts$/, loader: 'ts-loader' },
       { test: /\.css$/, loader: 'style!css?sourceMap' },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-      { test: /\.html$/, loader: "html" }
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.html$/, loader: 'html' }
     ]
   },
   noParse: [ /.+zone\.js\/dist\/.+/, /.+angular2\/bundles\/.+/ ]
@@ -37,7 +37,6 @@ function getIp() {
     adresses = interfaces['wlan0'];
   }
   
-    console.log('IP', interfaces);
   if (adresses) {
     ipV4 = adresses.filter(function (address) {
       return address.family === 'IPv4';
